9bd6d7eea82152b08dba8b3ba5991740ceae4675
IamQiuTian <devopsvip@163.com>
2017-02-27 13:37:49 +0800

    API数据变更监控

--- API_data_status_monitoring.py
+++ API_data_status_monitoring.py
@@ -4,6 +4,9 @@
 import requests
 import json
 import time
+import smtplib
+from email.mime.text import MIMEText
+
 
 class Json_code(object):
     def __init__(self,url,headers):
@@ -11,6 +14,12 @@ class Json_code(object):
         self.headers = headers
         self.old_json = {}
         self.new_json = {}
+
+        self.mailto_list=["收件人列表"]
+        self.mail_host="邮件服务器，例如smtp.163.com" 
+        self.mail_user="用户名"
+        self.mail_pass="密码"
+        self.mail_postfix="用户名后缀，例如163.com"
         
 
     def Retrieve_data(self):
@@ -23,11 +32,11 @@ class Json_code(object):
         self.old_json = t.Retrieve_data()["data"]["list"]
         time.sleep(300)
         self.new_json = t.Retrieve_data()["data"]["list"]
-        self.new_json["192.168.1.1"] = ["10","11"]
-        self.new_json["192.168.2.1"] = ["13","15"]
-        self.new_json["117.149.134.139"] = ["8","9"]
+        #self.new_json["192.168.1.1"] = ["10","11"]
+        #self.new_json["192.168.2.1"] = ["13","15"]
+        #self.new_json["117.149.134.139"] = ["8","9"]
         #self.old_json = {"127.0.0.1":["1","2","3"],"192.168.1.1":["2","3","4"]}
-        #self.new_json ={"127.0.0.1":["1","2","3"],"192.168.1.1":["2","3","4"],"168.54.24.1":["8"]}
+        #self.new_json = {"127.0.0.1":["1","2","3"],"192.168.1.1":["2","3","4"],"168.54.24.1":["8"]}
 
         l3 = []
         for key,value in  self.new_json.items():
@@ -51,6 +60,25 @@ class Json_code(object):
                 
         return dict3
 
+    def Send_mail(self,to_list,sub,content):
+        me="Qiudays"+"<"+self.mail_user+"@"+self.mail_postfix+">"  
+        msg = MIMEText(content,_subtype='plain',_charset='utf-8') 
+        msg["Accept-Language"]="zh-CN"
+        msg["Accept-Charset"]="ISO-8859-1,utf-8" 
+        msg['Subject'] = sub  
+        msg['From'] = me  
+        msg['To'] = ";".join(to_list)  
+        try:  
+            server = smtplib.SMTP()  
+            server.connect(self.mail_host)  
+            server.login(self.mail_user,self.mail_pass)  
+            server.sendmail(me, to_list, msg.as_string())  
+            server.close()  
+            return True  
+        except Exception, e:  
+            print str(e)  
+            return False 
+
     def main(self):
         while True:
             try:
@@ -61,21 +89,24 @@ class Json_code(object):
                         _tmp.append(k)
                         with open("tmp.log","a") as f:
                             for i in _tmp:
-                                f.write("{time} {ip} 的端口发生改变,现值是{new_code},原值是{old_code}\n\n".format(time=time.strftime("%Y-%m-%d %A %X %Z",time.localtime()),ip=i,new_code=self.new_json[i],old_code=self.old_json[i]))
-									
+                                self.text = "{time} {ip} 的端口发生改变,现值是{new_code},原值是{old_code}\n\n".format(time=time    .strftime("%Y-%m-%d %A %X %Z",time.localtime()),ip=i,new_code=self.new_json[i],old_code=self.old_json[i])
+                                f.write(self.text)
+                        t.Send_mail(self.mailto_list,"端口发生变化",self.text)
+
             except KeyError:
                 if _res:
                     for k,v in _res.items():
                         if k not in self.old_json:
+                            self.text2 = "{time} 新添加IP {ip}\n\n".format(time=time.strftime("%Y-%m-%d %A %X %Z",time.localtime()),ip=k)
                             with open("tmp.log","a") as f:
-                                f.write("{time} 新添加IP {ip}\n\n".format(time=time.strftime("%Y-%m-%d %A %X %Z",time.localtime()),ip=k)) 
-
+                                f.write(self.text2)
+                            t.Send_mail(self.mailto_list,"IP发生变化",self.text2)
+            break
                         
 if __name__ == "__main__":
     url = "https://x.x.x.x"
     headers = { 'API-KEY':'xxxx',
-                'API-SECRET':'yyyy',
-                'API-ID':'dddd'}
-					   
+                'API-SECRET':'xxxx',
+                'API-ID':'xxxx'}
     t = Json_code(url,headers)
     t.main()
