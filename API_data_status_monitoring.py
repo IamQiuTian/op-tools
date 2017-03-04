bbbbad91aaaf8104876b1ef32a09417ee5d59445
IamQiuTian <devopsvip@163.com>
2017-03-04 22:14:34 +0800

    接口数据监控

--- API_data_status_monitoring.py
+++ API_data_status_monitoring.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #-*- coding=utf-8 -*-
 
-import requests
+#import requests
+import urllib2
 import json
 import time
 import smtplib
@@ -15,26 +16,33 @@ class Json_code(object):
         self.old_json = {}
         self.new_json = {}
 
-        self.mailto_list=["收件人列表"]
-        self.mail_host="邮件服务器，例如smtp.163.com" 
-        self.mail_user="用户名"
-        self.mail_pass="密码"
-        self.mail_postfix="用户名后缀，例如163.com"
+        self.mailto_list=["xxx@163.com","yyy@163.com"]
+        self.mail_host="smtp.163.com" 
+        self.mail_user="user"
+        self.mail_pass="pass"
+        self.mail_postfix="163.com"
         
-
     def Retrieve_data(self):
-        _res = requests.get(self.url, headers=self.headers)
-        _data = json.loads(_res.text)
-        return _data
+        #_res = requests.get(self.url, headers=self.headers)
+        _reques = urllib2.Request(url=self.url,headers=self.headers)
+        _response = urllib2.urlopen(_reques)
+        _result = _response.read()
+        
+        _data = json.loads(_result)
+        if _data:
+            return _data
+        else:
+            t.Send_mail(self.mailto_list,"接口异常","接口获取数据为空!")
+            
 
     def Dictionary_intersection(self):
         dict3 = {}
         self.old_json = t.Retrieve_data()["data"]["list"]
-        #time.sleep(300)
+        time.sleep(1800)
         self.new_json = t.Retrieve_data()["data"]["list"]
         #测试IP不一致情况
         #self.new_json["192.168.1.1"] = ["10","11"]
-        self.old_json["192.168.2.1"] = ["13","15"]
+        #self.old_json["192.168.2.1"] = ["13","15"]
         #测试数据不一致情况
         #self.new_json["117.149.134.139"] = ["8","9"]
 
@@ -63,8 +71,8 @@ class Json_code(object):
                 dict3[key] = value
             else:
                 pass
-
         return dict3
+
 
     def Send_mail(self,to_list,sub,content):
         me="Qiudays"+"<"+self.mail_user+"@"+self.mail_postfix+">"  
@@ -75,17 +83,17 @@ class Json_code(object):
         msg['From'] = me  
         msg['To'] = ";".join(to_list)  
         try:  
-            server = smtplib.SMTP()  
-            server.connect(self.mail_host)  
-            server.login(self.mail_user,self.mail_pass)  
+            server = smtplib.SMTP_SSL()  
+            server.connect(self.mail_host, port=465)  
+            server.login(self.mail_user, self.mail_pass)  
             server.sendmail(me, to_list, msg.as_string())  
             server.close()  
             return True  
-        except Exception, e:  
-            print str(e)  
+        except Exception as e:  
+            print(str(e)) 
             return False 
 
-    def main(self):
+    def Main(self):
         while True:
             try:
                 _res = t.Dictionary_intersection()
@@ -95,7 +103,7 @@ class Json_code(object):
                         _tmp.append(k)
                         with open("tmp.log","a") as f:
                             for i in _tmp:
-                                self.text = "{time} {ip} 的端口发生改变,现值是{new_code},原值是{old_code}\n\n".format(time=time    .strftime("%Y-%m-%d %A %X %Z",time.localtime()),ip=i,new_code=self.new_json[i],old_code=self.old_json[i])
+                                self.text = "{time} {ip} 的端口发生改变,现值是{new_code},原值是{old_code}\n\n".format(time=time.strftime("%Y-%m-%d %A %X %Z",time.localtime()),ip=i,new_code=self.new_json[i],old_code=self.old_json[i])
                                 f.write(self.text)
                         t.Send_mail(self.mailto_list,"端口发生变化",self.text)
 
@@ -113,12 +121,12 @@ class Json_code(object):
                             with open("tmp.log","a") as f:
                                 f.write(self.text3)
                             t.Send_mail(self.mailto_list,"IP发生变化",self.text3)
-
-                        
+                            
+            
 if __name__ == "__main__":
     url = "https://x.x.x.x"
     headers = { 'API-KEY':'xxxx',
-                'API-SECRET':'xxxx',
+                'API-SECRET':'xxxxx',
                 'API-ID':'xxxx'}
     t = Json_code(url,headers)
-    t.main()
+    t.Main()
