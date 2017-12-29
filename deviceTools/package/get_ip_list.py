4aa190c0e292507a3dee01d2e51a98b538816699
iamqiutian <qiudays@163.com>
2017-12-29 15:28:58 +0800

    del

--- deviceTools/package/get_ip_list.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-def get_ip_list():
-    with open('/home/install/deviceTools/conf.d/device.list.setup','r') as f:
-        L=f.readlines()
-    ip_list=[]
-    for ip in L:
-        if "#" in ip:continue
-        ip_list.append(ip.strip())
-    return(ip_list)
